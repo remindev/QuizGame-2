c269b6f3be8e9f76cebcbc5db26fa0af5206e15a
remindev <reminremin01@gmail.com>
2023-02-02 16:27:15 +0530

    Final update

--- auth.js
+++ auth.js
@@ -1,8 +1,12 @@
-// import dotenv from 'dotenv'; // importing dot env   |
-//                                                 //  |  uncomment when at developement
-// dotenv.config(); // configuring dot env             |
-
 import bcript from 'bcryptjs'; // importing bcrypt to
+
+import * as appMain from './index.js'
+
+if(appMain.getAppconfig().isDev){ // this statement only runs if the is dev is true in appconfig
+    
+    import('dotenv').then(dotenv=>dotenv.config()); // importing and configuring and configuring dotenv 
+
+};
 
 import * as DBS from './schemas.js'; // improting skemas
 
@@ -51,144 +55,88 @@ export function randomId(length) {
  * @param {Request} request
  */
 export function createUser(request) {
-
     let email = request.body.email.trim(); // getting requested email 
-
     let password = request.body.password.trim(); // getting requested password
-
     let good = true; // flag to find errors
-
     // creating the promise as return type
-    return new Promise(async (resolve, reject) => { 
-
+    return new Promise(async (resolve, reject) => {
         // here try is used to find error form getting user data from db 
         try {
-
             let allUserEmail = await DBS.user.find({}).select({ _id: 0, email: 1 }); // gets all users email
-
             // formatting the data from db to an array of email
             allUserEmail.forEach((e, i, a) => {
                 allUserEmail[i] = e.email;
             });
-
             // checks if the email and password field is not empty
             if (email.lenght != 0 && password.length != 0) {
-
                 // checks if email from request is correctely formatted
                 if (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-
                     // checks the email form request is on the email list on db
                     allUserEmail.forEach((e, index, array) => {
-
                         if (e != email) {
-
                             // good | new email
-
                         } else {
-
                             // email existas
                             good = false; // error 
-
                             // reject promise with message
                             reject('email already exists');
-
                         };
-
                     });
-
                 } else {
-
                     // bad email fromat
-
                     good = false; // error
-
                     // rejects promis with message
                     reject("email is not valid");
                 };
-
                 // after checking email checks the length of passeword is vlaid or not
                 if (password.length >= 6) {
-
                     // good
-
                 } else {
                     // password lenght is lesser than theh requirement
-
                     good = false; // error
-
                     // rejects promise with message
                     reject('password length must be 6');
                 };
-
             } else {
-
                 // one of the fields or both of the fields is empty
-
                 good = false; // error
-
                 // rejects promise with message
                 reject("email or password field cannot be empty");
-                
             };
-
             // if there is no false is marked | no error is happen the user creation moves to next step
             if (good) {
-
                 UID_CREATOR(); // calls 
-
-                async function UID_CREATOR() { 
-
+                async function UID_CREATOR() {
                     // creats a unique user id for every new user
-
                     // gets all uid from db
                     let UID = await DBS.user.find({}).select({ _id: 0, UID: 1 });
-
                     let userID = randomId(25); // creates a random string for id;
-
                     let flag = 2; // flag for find if new id created is a duplicate id or not | 2 == good
-
-                    UID.forEach((e, i, a) => { 
-
+                    UID.forEach((e, i, a) => {
                         // this loop finds and update duplicate is there or not
-
                         if (e.UID == userID) {
                             flag = 1; // if duplicate id
                         };
-
                     });
-
                     if (flag == 2) {
                         // if new unique id 
-
                         // saves user to db
                         savesUser(userID);
-
                     } else {
                         // if duplicate id of user id is found
-
                         // re-run this function 
                         return UID_CREATOR();
-
                     };
-
-
                 };
-
                 // saves user to db
                 function savesUser(userID) {
-
                     let saltRounds = 10; // salt rounds 
-
                     // making hash of password from client
                     bcript.hash(password, saltRounds, async function (err, hash) {
-
-                        if (err) { 
-                            
+                        if (err) {
                             // error handling
                             console.error(err);
-
                         } else {
-
                             // creates user object
                             let user = await DBS.user({
                                 email: email,
@@ -196,274 +144,154 @@ export function createUser(request) {
                                 level: 1,
                                 UID: userID
                             });
-
                             user.save(); // saves to db
-
                             resolve(user); // resolves the promise with user object
-
                         };
-
                     });
-
                 };
-
             };
-
         } catch (e) {
-
             // error from data reading | DB
-
             console.error(e); // logs error
-
         };
-
     });
-
 };
-
-
 export function loginUser(request) {
-
     // this function logs in a user by the email existis on db
-
     let email = request.body.email.trim(); // getting requested email 
-
     let password = request.body.password.trim(); // getting requested password 
-
     let good = true; // flag to find errors
-
     // returning a promise as a result of this function
     return new Promise(async (resolve, reject) => {
-
         // getting user data from db 
         let emailList = await DBS.user.find({}).select({ _id: 0, email: 1 }); // email of all useres
-
         // formatting to an array the data from db
         emailList.forEach((e, i, a) => {
             emailList[i] = e.email;
         });
-
         // validating inputs
         if (email.length > 3 && password.length >= 6) {
-
             if (email.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
-
                 let emailNotExist = true;
-
                 emailList.forEach(e => {
-
                     if (e == email) {
-
                         // if the email client entered is on the db
-
                         // you are good to go
                         emailNotExist = false;
-
                     };
-
                 });
-
                 if (emailNotExist == true) {
-
                     // if the email is not on the server 
-
                     // reject promise with error message
                     reject("Acoount not exist on this Email");
-
                 } else {
-
                     // valid mail
-
                     // getting user data on db by the email user entered
                     let user = await DBS.user.find({ email: email }).select({ _id: 0 });
-
                     // comparing the passwoed user entered by the hash stored in db
                     bcript.compare(password, user[0].password, async (err, sucess) => {
-
                         if (err) {
-
                             // comparing password error handling
-
                             console.error(err);
-
                         } else {
-
                             // if there is no error
                             if (sucess) {
-
                                 // if client entered password matches the hash on server
-
                                 // resolve promise with the user data as the parameter
                                 resolve(user[0]);
-
                             } else {
-
                                 // when password and hash dosent match
-
                                 // reject promise with an error message
                                 reject("Incorrect Password");
-
                             };
-
                         };
-
                     });
-
                 }
-
             } else {
                 // is the email is not valid
-
                 good = false; // error
-
                 // reject promise with error message
                 reject('Enter is not valid');
             }
-
         } else {
-
             // one of the field or both dosent meet the required length 
-
             good = false; // error
-
             // rejecting promise with error message
             reject("Email or password dosn't meet the required length");
         };
-
     });
-
 };
-
-
 // auth check function to check if user is logged in or not
-
 export async function authInitalCheck(req, res, next) {
-
     // if user object is on the session obj, that means user is logged in
     if (req.session.user) {
-
         req.isLoggedIn = true; // setting isLoggedIn variable inside req object as true to indicate the presence of logged in user
-
         if (req.session.user.isGuest == false) { // check if user is guest user or not
-
             let UID = req.session.user.UID; // getting user id from existing sesson
-
-             //if client is an email user the clients data stored in db is collected form here and added to main req object
-
+            //if client is an email user the clients data stored in db is collected form here and added to main req object
             let user = await DBS.user.find({ UID: UID }); // from db
-
             req.user = { // adding user data to main req object
                 level: user[0].level,
                 email: user[0].email,
                 UID: user[0].UID,
                 isGuest: false
             };
-
         } else {
-
             // if user is a guest
-
             // settig req.user as the data from the sesson 
-
             req.session.user.email = "Guest"; // inted of email we write guest
             req.user = req.session.user; // user data
-
         };
-
     } else {
-
         req.isLoggedIn = false; // setting isLoggedIn variable inside req object as false to indicate the absence of logged in user
-
     };
-
     next(); // move on to the next funciton | middle ware
-
 };
-
-
-
-
 /*
    This function is resposible to redirect user to the login page when the user is not logged in
    If logged in this function just passes and move to next funtion
 */
 export function mustLogin(req, res, next) {
-
     if (req.isLoggedIn == false) {
-
         // is there is no user is logged in, client is redirected to login page
-
         res.redirect('/login');
-
     } else {
-
         next(); // moves on to next function 
-
     };
-
 };
-
 export function mustLoginApi(req, res, next) {
-
     // this funciton is same as the functoin mustlogin above the difference is theat this funciton is not redirecting user to login page
     // isted this give an error message as result
-
     if (req.isLoggedIn == false) {
-
         // if the user is not logged in 
-
         // send error message responce to user
         res.send({ status: 'error', message: 'access denied' }).status(403);
-
     } else {
-
         next();
-
     };
-
 };
-
 /*
    This function is resposible to redirect user to the home page when the user is logged in
    If not logged in, this function just passes and move to next funtion
 */
 export function mustLogout(req, res, next) {
-
     if (req.isLoggedIn == true) {
-
         // user is loged in 
-
         // redirect user to home page
         res.redirect('/');
-
     } else {
-        
         // user is not logged in 
-
         next(); // moves on to next function 
-
     };
-
 };
-
 export function mustLogoutApi(req, res, next) {
-
     // this funciton is same as the funciton above mustLogout the only difference is that insted to redirectng user to home page this send's a message to client
     if (req.isLoggedIn == true) {
-
         // user is logged in 
-
         // sends an error message to client
         res.send({ status: 'error', message: 'access denied - Already logged in', code: 403771, type: req.user.isGuest, email: req.user.email }).status(403);
-
     } else {
-
         // user is not logged in 
-
         next(); // moves to next function 
-
     };
-
 };
-
-
--- schemas.js
+++ schemas.js
@@ -1,54 +1,30 @@
 import mongoose from "mongoose"; // importing mongoose
-
-// import dotenv from 'dotenv'; // importing dot env   |
-//                                                 //  |  uncomment when at developement
-// dotenv.config(); // configuring dot env             |
+import dotenv from 'dotenv'; // importing dot env 
+dotenv.config(); // configuring dot env             |
 
 var DBDelay = new Date;
-
-
 // auth db | USER
-
 const AuthDB = mongoose.createConnection(process.env.USERDB_URL); // connecting to db
-
 AuthDB.on('error', (error) => console.log(error)); // checks for errors
-
 AuthDB.once('open', () => logger(new Date,"")); // run once after the db is connected
 
-
-
 // games db | PROJECTS
-
 const dbLevel = mongoose.createConnection(process.env.LEVELDB_URL); // connecting to db
-
 dbLevel.on('error', (error) => console.error(error)); // checks for errors
-
 dbLevel.once('open', () => logger("",new Date) ); // run once after the db is connected
 
-
 // printing connnection status of each db
-
 var DBNumber = 2; // number of databases
-
 var DBConnected = 0; // number of ba connected
-
 var DBTimeRemorder = {}; // time data for db connectied
-
 function logger(auth,game){
-
     DBTimeRemorder.authTime = auth!=""?auth:DBTimeRemorder.authTime; // updating time taken to connect to user db
     DBTimeRemorder.gameTime = game!=""?game:DBTimeRemorder.gameTime; // updating time taken to connect to game db
-
     if(++DBConnected == DBNumber){ // if both db's time data is arrived 
-
         // logs the time taken to connected to console
         console.log(`[-] DB connected in : Auth ${DBTimeRemorder.authTime-DBDelay}ms, Game ${DBTimeRemorder.gameTime-DBDelay}ms`);
-
     };
-
 };
-
-
 /**
  * user schema 
  */
@@ -69,10 +45,7 @@ const userSchema = new mongoose.Schema({
     nameID: String,
     UID: String
 });
-
 export let user = AuthDB.model('users', userSchema); // exports user schema | db - users
-
-
 /**
  * game level schema
  */
@@ -82,5 +55,4 @@ const codeGame = new mongoose.Schema({
     question: String,
     answer: String
 });
-
 export let levels = dbLevel.model('levels', codeGame); // exports games schema | db - projects
